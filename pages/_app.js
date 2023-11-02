cb9c8990a0aabea3bd1dbcb22feb7a1f1f789a0f
Johan Lundqvist <>
2023-11-02 16:13:39 +0100

    Fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 'use client';
 
-import Head from 'next/head';
 import { Accordion as A } from '../components/accordion/basic/Accordion';
 import { Accordion as B } from '../components/accordion/a11y/Accordion';
 

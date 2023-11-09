d6551854cf0734bcc93d1208813a50b554c7aa41
Johan Lundqvist <>
2023-11-09 15:30:03 +0100

    Add dockerfile for prod build

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Accessible accordion</title>
-        <meta property="og:title" content="Accessible accordion" key="title" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { AccordionContainer } from '../components/accordion/AccordionContainer';
 import { ToastWrapper } from '../components/toast/ToastWrapper';
 import { FormWrapper } from '../components/form/FormWrapper';
+import Head from 'next/head';
 
 const HomePage = ({ data }) => {
   return (
     <>
+      <Head>
+        <title>Accessible components</title>
+        <meta property="og:title" content="Accessible accordion" key="title" />
+      </Head>
       <header>
         <h1 style={{ marginBottom: '1em' }}>Accessible components</h1>
       </header>
       <main style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>
-        <h2>Hello: {JSON.parse(data).time}</h2>
+        <p>The time is: {JSON.parse(data).time}</p>
         <AccordionContainer />
         <ToastWrapper />
         <FormWrapper />
@@ -20,7 +25,10 @@ const HomePage = ({ data }) => {
 };
 
 export async function getServerSideProps() {
-  const data = JSON.stringify({ time: new Date().toLocaleDateString() });
+  const data = JSON.stringify({
+    time:
+      new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString(),
+  });
   return { props: { data } };
 }
 
